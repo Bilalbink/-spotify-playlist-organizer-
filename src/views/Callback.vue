01a51d54eb364ea9ac29f06518e873ff9db12164
Muhammad Bilal Bin Khalid <bilalbink1@gmail.com>
2021-05-22 19:53:11 +0300

    removed logs

--- src/views/Callback.vue
+++ src/views/Callback.vue
@@ -8,13 +8,11 @@
 export default {
     name: "Callback",
     created () {
-        // console.log(this.$route.hash);
         var result = this.$route.hash.split('&').reduce(function (res, item) {
             var parts = item.split('=');
             res[parts[0]] = parts[1];
             return res;
         }, {});
-        console.log(result);
         localStorage.ACCESS_TOKEN = result['#access_token'];
         this.$router.replace({path: '/playlist'});
     }
